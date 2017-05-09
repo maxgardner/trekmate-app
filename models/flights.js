2ccee97294f735e7d44b78ffadece2f6b52eae1c
Roman Brito <Roman Brito>
2017-05-08 21:29:14 -0500

    roman

--- models/flights.js
+++ models/flights.js
@@ -1,37 +1,29 @@
-module.exports = function(sequelize, DataTypes) { 
-    var Flight = sequelize.define("Flights", { 
-        id: { 
-            type: DataTypes.INTEGER, 
-            allowNull: false, 
-            primaryKey: true,
-            autoIncrement: true
-         }, 
-         flight_number: { 
-             type: DataTypes.INTEGER, 
-             allowNull: false
-         }, 
-         city_departure: { 
-             type: DataTypes.INTEGER, 
-             allowNull: false
-         },
-         arrival_time: { 
-             type: DataTypes.TIME,
-             allowNull: true
+module.exports = function (sequelize, Datatypes) {
+    var Flight = sequelize.define("Flight", {
+            flight_number: {
+                type: Datatypes.STRING,
+                allowNull: false
+            },
+            flight_date: {
+                type: Datatypes.DATEONLY,
+                allowNull: true
+            },
+            city_departure: {
+                type: Datatypes.STRING,
+                allowNull: true
             }
-         },
-         { 
-             classMethods: { 
-                 associate: function(models) { 
-                    Flight.belongsTo(models.Trip, 
-                    {
-                        onDelete: "cascade", 
-                        foreignKey: { 
-                            allowNull: false
-                            } 
-                        }); 
-                    }
-                 }
-             }); 
-        
+        },
+        {// Associations
+            classMethods: {
+                associate: function (models) {
+                    Flight.belongsTo(models.Trip, {
+                        foreignKey: {
+                            allowNull: true
+                            // foreignKey created TripId
+                        }
+                    });
+                }
+            }
+        });
     return Flight;
 };
