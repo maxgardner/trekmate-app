e97a9cd0f1b5373c3c98305c9fd834b558a20dff
Roman Brito <Roman Brito>
2017-05-12 23:02:40 -0500

    passing trip info

--- models/carRental.js
+++ models/carRental.js
@@ -1,16 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
     var CarRental = sequelize.define("Car_Rental", {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-         },
          Rental_Comp: {
              type: DataTypes.STRING,
                     allowNull: false,
                     validate: { len: [1]}
-         },
+         }
     },
     {
         classMethods: {
--- routes/trip.js
+++ routes/trip.js
@@ -1,41 +1,77 @@
 module.exports = function(router, db) {
-  router.get("/trip/:id?", function (req, res) {
-    var tripId;
-    if (req.params.id) {
-      tripId = req.params.id;
-    } else {
-      tripId = req.body.uuid;
-    }
-    db.Trip.findOne({
-      where: {
-        uuid: tripId
-      },
-      include: [db.User],
-      include: [db.Destination],
-      include: [db.Flight],
-      include: [db.Hotel],
-      include: [db.Car_Rental],
-      include: [db.Activity]
-    })
-    .then(function(tripInfo) {
-      console.log("Trip Info returned: " + JSON.stringify(tripInfo));
-      // var info = {
-      //   trip: tripInfo,
-      //   user: tripInfo.User,
-      //   destination: tripInfo.Destination,
-      //   flight: tripInfo.Flight,
-      //   hotel: tripInfo.Hotel,
-      //   carRental: tripInfo.CarRental,
-      //   activity: tripInfo.Activity
-      // };
-      // res.render("tripinfo", info);
-        res.json(tripInfo);
+    router.get("/trip/:id?", function (req, res) {
+        var tripId;
+        if (req.params.id) {
+            tripId = req.params.id;
+        } else {
+            tripId = req.body.uuid;
+        }
+        db.Trip.findOne({
+            where: {
+                uuid: tripId
+            }
+        })
+            .then(function(dbTrip) {
+                var trip = dbTrip;
+                db.Activity.findAll({
+                    where: {
+                        TripUuid: tripId
+                    }
+                })
+                    .then(function(dbActivity) {
+                        var activity = dbActivity;
+                        db.Car_Rental.findAll({
+                            where: {
+                                TripUuid: tripId
+                            }
+                        })
+                            .then(function(dbCar) {
+                                var carRental = dbCar;
+                                db.Flight.findAll({
+                                    where: {
+                                        TripUuid: tripId
+                                    }
+                                })
+                                    .then(function(dbFlight) {
+                                        var flight = dbFlight;
+                                        db.Hotel.findAll({
+                                            where: {
+                                                TripUuid: tripId
+                                            }
+                                        })
+                                            .then(function(dbHotel) {
+                                                var hotel = dbHotel;
+                                                db.User.findOne({
+                                                    where: {
+                                                        id: req.user.id
+                                                    }
+                                                })
+                                                    .then(function(dbUser) {
+                                                        var user = dbUser;
+
+                                                        var info = {
+                                                            trip: trip,
+                                                            user: user,
+                                                            flight: flight,
+                                                            hotel: hotel,
+                                                            carRental: carRental,
+                                                            activity: activity
+                                                        }
+
+                                                        console.log(info);
+                                                        res.json(info);
+                                                        //res.render("tripinfo", info);
+                                                    });
+                                            });
+                                    });
+                            });
+                    });
+            });
     });
-  });
 
-  router.get("/trips", function(req, res) {
-    res.render("tripinfo");
-  });
+    router.get("/trips", function(req, res) {
+        res.render("tripinfo");
+    });
 
-  return router;
+    return router;
 };
